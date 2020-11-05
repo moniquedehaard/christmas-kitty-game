7b490b8d4dff3a59afc370b144ead293c2df5983
Monique de Haard <moniquedehaard@msn.com>
2020-11-05 10:15:59 +0100

    maps correct

--- src/main.js
+++ src/main.js
@@ -15,19 +15,19 @@ button.addEventListener('click', function() {
 ////// Game //////
  function preload() {
   //player
-  bgImage = loadImage("/images/BG.jpg");
-  player = loadImage("/images/Christmas Kitty.png");
-  santa = loadImage("/images/Santa's slee.png");
-  heart =loadImage("/images/heart.png");
+  bgImage = loadImage("./images/BG.jpg");
+  player = loadImage("./images/Christmas Kitty.png");
+  santa = loadImage("./images/Santa's slee.png");
+  heart =loadImage("./images/heart.png");
 
   // create obstacles array elements
-  obstaclesPic[0] = loadImage("/images/Tree.png");
-  obstaclesPic[1] = loadImage("/images/Reindeer.png");
-  obstaclesPic[2] = loadImage("/images/snowman.png");
+  obstaclesPic[0] = loadImage("./images/Tree.png");
+  obstaclesPic[1] = loadImage("./images/Reindeer.png");
+  obstaclesPic[2] = loadImage("./images/snowman.png");
 
   // create presents array
-  presentsPic[0] = loadImage("/images/present1.png");
-  presentsPic[1] = loadImage("/images/present2.png");
+  presentsPic[0] = loadImage("./images/present1.png");
+  presentsPic[1] = loadImage("./images/present2.png");
 }
 
 
